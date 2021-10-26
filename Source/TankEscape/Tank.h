43ff78adc87e980e06f7c71681ed6df6b0a348b5
Brandon Mitchell <83912617+Sanda2399@users.noreply.github.com>
2021-10-26 13:10:36 -0400

    Created BasePawn and Tank classes

---
+++ Source/TankEscape/Tank.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class TANKESCAPE_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+	ATank();
+
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCameraComponent* Camera;	
+};
